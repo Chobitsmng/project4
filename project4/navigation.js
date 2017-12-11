9a258f7f99099adfe780bda0bade9be3563b408c
Leslie <chobitsmng@gmail.com>
2017-12-11 00:19:39 -0500

    uploaded flies
    work in progress final project for html5 college class

---
+++ project4/navigation.js
@@ -0,0 +1,3 @@
+document.write("<div style='text-align: center;'>")
+document.write("<a href='index.htm'>Home</a> - <a href='galleries.html'>Galleries</a> - <a href='submitrules.html'>Submit/Rules</a> - <a href='topics.html'>Topics</a> - <a href='vote.html'>Vote</a>");
+document.write("</div>")
